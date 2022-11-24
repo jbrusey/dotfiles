b2f6f120aac0d5bd2d65f43fe12d81b3b6a81721
James Brusey <james.brusey@gmail.com>
2022-11-24 13:06:00 +0000

    no idea

--- .config/crafted-emacs/config.el
+++ .config/crafted-emacs/config.el
@@ -22,10 +22,6 @@
 (require 'crafted-windows)
 (require 'crafted-org)
 
-;; Set further font and theme customizations
-(custom-set-variables
-   '(crafted-ui-default-font
-     '(:font "JetBrains Mono" :weight light :height 185)))
 
 (load-theme 'modus-operandi t)
 
@@ -60,6 +56,133 @@
 (straight-use-package 'magit)
 ;; I had this previously but I'm not convinced it is needed or useful.
 ;; (customize-set-variable magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Org Mode Appearance ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Set further font and theme customizations
+(custom-set-variables
+   '(crafted-ui-default-font
+     '(:font "JetBrains Mono" :weight light :height 180)))
+
+(defvar my/variable-width-font "Iosevka Aile"
+  "The font to use for variable pitch (document) text.")
+
+(set-face-attribute 'variable-pitch nil :font my/variable-width-font :weight 'light :height 1.1)
+
+(require 'org-faces)
+
+;; Resize Org headings
+(dolist (face '((org-level-1 . 1.2)
+                (org-level-2 . 1.1)
+                (org-level-3 . 1.05)
+                (org-level-4 . 1.0)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font my/variable-width-font :weight 'medium :height (cdr face)))
+
+;; Make the document title a bit bigger
+(set-face-attribute 'org-document-title nil :font my/variable-width-font :weight 'bold :height 1.3)
+
+;; Make sure certain org faces use the fixed-pitch face when variable-pitch-mode is on
+(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-formula nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; visual-fill-column package ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(straight-use-package 'visual-fill-column)
+
+;; Configure fill width
+(setq visual-fill-column-width 110
+      visual-fill-column-center-text t)
+
+;;;;;;;;;;;;;;;;;
+;; org-present ;;
+;;;;;;;;;;;;;;;;;
+
+(straight-use-package 'org-present)
+
+(defun my/org-present-prepare-slide (buffer-name heading)
+  ;; show only top-level headlines
+  (org-overview)
+
+  ;; Unfold the current entry
+  (org-show-entry)
+
+  ;; Show only direct subheadings of the slide but don't expand them
+  (org-show-children)
+  )
+
+(defun my/org-present-start ()
+  "Tweak font sizes"
+  (setq-local face-remapping-alist '((default (:height 1.5) variable-pitch)
+                                     (header-line (:height 4.0) variable-pitch)
+                                     (org-document-title (:height 1.75) org-document-title)
+                                     (org-code (:height 1.55) org-code)
+                                     (org-verbatim (:height 1.55) org-verbatim)
+                                     (org-block (:height 1.25) org-block)
+                                     (org-block-begin-line (:height 0.7) org-block)))
+
+  ;; Set a blank header line string to create blank space at the top
+  (setq header-line-format " ")
+
+  ;; Display inline images automatically
+  (org-display-inline-images)
+
+  ;; Center the presentation and wrap lines
+  (visual-fill-column-mode 1)
+  (visual-line-mode 1))
+
+(defun my/org-present-end ()
+  ;; Reset font customizations
+  (setq-local face-remapping-alist '((default variable-pitch default)))
+
+  ;; Clear the header line string so that it isn't displayed
+  (setq header-line-format nil)
+
+  ;; Stop displaying inline images
+  (org-remove-inline-images)
+
+  ;; Stop centering the document
+  (visual-fill-column-mode 0)
+  (visual-line-mode 0))
+
+;; Turn on variable pitch fonts in Org Mode buffers
+(add-hook 'org-mode-hook 'variable-pitch-mode)
+
+;; Register hooks with org-present
+(add-hook 'org-present-mode-hook 'my/org-present-start)
+(add-hook 'org-present-mode-quit-hook 'my/org-present-end)
+(add-hook 'org-present-after-navigate-functions 'my/org-present-prepare-slide)
+
+
+
+
+;; (add-hook 'org-present-mode-hook
+;;           (lambda ()
+;;             (org-present-big)
+;;             (org-display-inline-images)
+;;             (org--latex-preview-region (point-min) (point-max))
+;;             (org-present-hide-cursor)
+;;             (org-present-read-only)
+;;             ))
+;; (add-hook 'org-present-mode-quit-hook
+;;           (lambda ()
+;;             (org-present-small)
+;;             (org-remove-inline-images)
+;;             (org-present-show-cursor)
+;;             (org-present-read-write)))
 
 
 (load "custom")
